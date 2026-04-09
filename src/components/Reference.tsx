d983bca9584401e62cedb375c463cd257f31893e
DATOTO <110515969+DATOT@users.noreply.github.com>
2026-04-09 21:05:16 +0700

    Reference - CatraHN

--- src/components/Reference.tsx
+++ src/components/Reference.tsx
@@ -4,17 +4,15 @@ const Reference: React.FC = () => {
   return (
     <header className="w-full border-b bg-yellow-200 shadow-sm h-[70%]">
       <div className="max-w-6xl mx-auto px-4 py-4 flex flex-col gap-1">
-        <h1 className="text-2xl font-bold text-gray-800">
-          {/* Điền tiêu đề ở đây */}
-        </h1>
+        <h1 className="text-2xl font-bold text-gray-800">Reference Title</h1>
 
         {/* Subtitle */}
-        <p className="text-sm text-gray-500">{/* Điền bất kì ở đây */}</p>
+        <p className="text-sm text-gray-500">Cá tra HN</p>
 
         {/* Author */}
         <p className="text-xs text-gray-400 mt-1">
           Làm bởi:
-          <span>{/* Điền tên ở đây */}</span>
+          <span>Lâm Tiến Đạt</span>
         </p>
       </div>
     </header>
